16800007393499890ef93ce506355ffe605d5ebc
Swathi <swathy.m.ram@gmail.com>
2026-01-02 17:21:32 +0530

    Update RunningSum.java

--- Arrays/RunningSum.java
+++ Arrays/RunningSum.java
@@ -1,13 +1,24 @@
+/*
+ * Problem: Running Sum of 1d Array (LeetCode Easy)
+ * Approach: Prefix Sum
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
 public class RunningSum {
-  public int[] runningSum(int[] nums) {
+
+    public int[] runningSum(int[] nums) {
         if (nums == null || nums.length == 0) {
-            return new int[0];                }
+            return new int[0];
+        }
+
         int[] sums = new int[nums.length];
         sums[0] = nums[0];
-       for (int i = 1; i < nums.length; i++) {
+
+        for (int i = 1; i < nums.length; i++) {
             sums[i] = nums[i] + sums[i - 1];
-       }
+        }
 
         return sums;
     }
+}
 }
