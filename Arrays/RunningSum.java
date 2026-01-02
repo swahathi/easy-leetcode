46df09a90084b33e50bd4e71ea279b94f208a8e0
Swathi <swathy.m.ram@gmail.com>
2026-01-02 17:17:44 +0530

    Add Running Sum of Array solution

---
+++ Arrays/RunningSum.java
@@ -0,0 +1,13 @@
+public class RunningSum {
+  public int[] runningSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];                }
+        int[] sums = new int[nums.length];
+        sums[0] = nums[0];
+       for (int i = 1; i < nums.length; i++) {
+            sums[i] = nums[i] + sums[i - 1];
+       }
+
+        return sums;
+    }
+}
